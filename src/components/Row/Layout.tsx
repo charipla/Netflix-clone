c558eab5500f92cd7322d15a37b0b84d0561f26a
Keisuke Funaya <keisukefunaya@KeisukenoMac-mini.local>
2024-11-13 19:28:37 +0900

    add accordion animation

--- src/components/Row/Layout.tsx
+++ src/components/Row/Layout.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { Movie } from "../../type.ts";
 import YouTube from "react-youtube";
 
@@ -40,7 +41,7 @@ export const Layout = ({
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className={`object-contain w-full max-h-24 m-2 transform transition-transform duration-450 ${
+            className={`object-contain w-full max-h-24 m-2 transform transition-transform duration-450 cursor-pointer ${
               isLargeRow ? "max-h-60 hover:scale-110" : "hover:scale-108"
             }`}
             src={`${image_url}${
@@ -51,7 +52,19 @@ export const Layout = ({
           />
         ))}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      <AnimatePresence>
+        {trailerUrl && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.5 }}
+            className="mt-5"
+          >
+            <YouTube videoId={trailerUrl} opts={opts} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
